97ac269da3fe87e43f9347ab0f1894e3b9c580a5
Henry <hbravo@millionandup.com>
2023-10-14 15:40:35 -0500

    Remove bin, obj

---
+++ Backend/N5/src/User/N5.Microservice.User/N5.User.Domain/DTO/ModifyPermissionCompleteDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N5.User.Domain.DTO
+{
+    internal class ModifyPermissionCompleteDTO
+    {
+    }
+}
---
+++ Backend/N5/src/User/N5.Microservice.User/N5.User.Domain/DTO/ModifyPermissionDto.cs
@@ -0,0 +1,44 @@
+﻿using N5.Domain.Interfaces.DTO;
+
+namespace N5.User.Domain.DTO;
+    /// <summary>
+    /// Modify User permission
+    /// </summary>
+    public class ModifyPermissionDto : IPayloadMessage
+{
+    /// <summary>
+    /// User's Session
+    /// </summary>
+    public string IdSession { get; set; }
+    /// <summary>
+    /// Determine what is the current operation
+    /// </summary>
+    public string NameOperation { get => "modify"; }
+    /// <summary>
+    /// Session Id
+    /// </summary>
+    public int  Id{ get; set; }
+
+    /// <summary>
+    /// Employee name
+    /// </summary>
+    public string EmployeeForename { get; set; }
+
+    /// <summary>
+    /// Employee name
+    /// </summary>
+    public string EmployeeSurname { get; set; }
+
+    /// <summary>
+    /// Permission type Id Id
+    /// </summary>
+    public int PermissionTypeId { get; set; }
+
+    /// <summary>
+    /// Permission Date
+    /// </summary>
+    public DateTime PermissionDate { get; set; }
+
+
+
+}
